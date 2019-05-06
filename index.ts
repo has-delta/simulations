3b156996a4e3e2265d183db143c9da19647edfd2
손량 <loop.infinitely@gmail.com>
2019-05-06 12:31:45 +0900

    added navigation... again

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+import {MDCDrawer} from '@material/drawer';
+import {MDCTopAppBar} from '@material/top-app-bar';
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+topAppBar.setScrollTarget(document.getElementById('main-content'));
+topAppBar.listen('MDCTopAppBar:nav', () => {
+    drawer.open = !drawer.open;
+});
