82f4e36001647e2436413d908ab960d19a93f4e0
손량 <loop.infinitely@gmail.com>
2019-05-07 12:08:10 +0900

    two spaces indent

--- index.ts
+++ index.ts
@@ -4,5 +4,5 @@ const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
 topAppBar.setScrollTarget(document.getElementById('main-content'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
-    drawer.open = !drawer.open;
+  drawer.open = !drawer.open;
 });
