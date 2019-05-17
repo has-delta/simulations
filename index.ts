a9358dc3e45c99640591471d834ade473cc0e449
손량 <loop.infinitely@gmail.com>
2019-05-18 00:05:30 +0900

    added ajax to drawer

--- index.ts
+++ index.ts
@@ -7,3 +7,20 @@ topAppBar.setScrollTarget(document.getElementById('main-content'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
   drawer.open = !drawer.open;
 });
+
+const indexUrl = 'https://raw.githubusercontent.com/has-delta/simulation-data/master/index.json';
+fetch(indexUrl)
+.then(res => res.json())
+.then(data => {
+  const topics = data.topics;
+  console.log(topics);
+  const drawerElem = document.getElementById('drawer');
+  for (const topic of topics) {
+    const topicElem = document.createElement('a');
+    topicElem.setAttribute('class', 'mdc-list-item');
+    topicElem.setAttribute('href', '#');
+    topicElem.setAttribute('aria-current', 'page');
+    topicElem.text = topic.desc.ko;
+    drawerElem.appendChild(topicElem);
+  }
+});
