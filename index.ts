3cfbbae0261d3ce466d319653f4eb3a89564b83e
손량 <loop.infinitely@gmail.com>
2019-05-17 22:50:36 +0900

    formatted

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import {MDCDrawer} from '@material/drawer';
 import {MDCTopAppBar} from '@material/top-app-bar';
+
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
 topAppBar.setScrollTarget(document.getElementById('main-content'));
